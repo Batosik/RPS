b25cf266d74b4fff6fd90a2dd3db11cba959d15e
Erokhin <8batos@gmail.com>
2023-12-22 02:59:57 +0300

    Добавил немного экшена

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import readlineSync from 'readline-sync/lib/readline-sync.js';
 import getRandom from './getRandom.js';
+import { setTimeout } from "timers/promises"
 
 let count = [0, 0];
+const tools = ['Rock', 'Paper', 'Scissors'];
 
 const rps = (count) => {
   let currantUserCount = count[1];
@@ -26,19 +28,24 @@ const rps = (count) => {
 
 console.log("Let's play rock-paper-scissors");
 
-const tools = ['Rock', 'Paper', 'Scissors'];
+await setTimeout(400, console.log('Rock'))
+await setTimeout(400, console.log('Paper'))
+await setTimeout(400, console.log('Scissors'))
 
-rps(count);
+rps(count)
 
-console.log('Play til three wins');
+await setTimeout(1000, console.log('Play til three wins')) 
 
 while (count[0] < 4 || count[1] < 4) {
+  await setTimeout(400, console.log('Rock'))
+  await setTimeout(400, console.log('Paper'))
+  await setTimeout(400, console.log('Scissors'))
   rps(count);
   if (count[0] === 3) {
-    console.log("Let's try again");
-    break;
+    await setTimeout(1000, console.log("Let's try again"))
+    break
   } else if (count[1] === 3) {
-    console.log('My congratulations!');
-    break;
+    await setTimeout(1000, console.log('My congratulations!'))
+    break
   }
 }
